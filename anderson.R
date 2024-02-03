65c38c89584ce199bd04c0a9cf89fef808576ad7
Ed Hagen <ehagen@gmail.com>
2024-02-03 12:51:46 -0800

    Many edits, including addition of rater2 and new ggplots

--- anderson.R
+++ anderson.R
@@ -1,10 +1,12 @@
 #+ message=F
 
 library(tidyverse)
+library(vcd)
 library(knitr)
 library(readxl)
+library(patchwork)
 
-# Anderson et al. (2023)
+# Anderson et al. (2023) ACNRW
 # https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0287101
 
 # Uncomment to download excel data file from Anderson et al. (2023)
@@ -17,7 +19,19 @@ anderson <-
     anderson_preysize = `small (1), medium (2), large game (3), all (4)`,
     anderson_hunt = `Documentation of women hunting? (0=no, 1=yes)`
   ) |> 
-  dplyr::select(anderson_subsistence, anderson_preysize, anderson_hunt)
+  mutate(
+    anderson_subsistence2 = case_when(
+      anderson_subsistence == '0' ~ 'Gathering',
+      anderson_subsistence == '1' ~ 'Hunting',
+      anderson_subsistence == '2' ~ 'Fishing',
+      anderson_subsistence == '1,2' ~ 'Hunting/Fishing',
+      anderson_subsistence == '3' ~ 'Hunting/Gathering',
+      .default = NA
+    )
+  ) |> 
+  dplyr::select(anderson_subsistence, anderson_subsistence2, anderson_preysize, anderson_hunt)
+
+hunt_freq <- c('No evidence', 'Never', 'Rarely', 'Sometimes', 'Frequently')
 
 recode <- 
   read_excel('recoding.xlsx', skip = 1, na = 'N/A') |> 
@@ -30,17 +44,21 @@ recode <-
   bind_cols(anderson) |> # fragile, but the rows are in the same order
   mutate(
     anderson_hunt2 = ifelse(anderson_hunt == 0, 'Anderson et al.: No', 'Anderson et al.: Yes'),
-    anderson_preysize = ifelse(Society == 'Tamang', NA, anderson_preysize),
+    anderson_smallgame = anderson_preysize %in% c('1', '2'),
+    anderson_largegame = anderson_preysize %in% c('3', '4'),
     anderson_preysize2 = case_when(
-      anderson_preysize %in% c('1', '2') ~ 'Anderson et al.: Small/medium game',
-      anderson_preysize %in% c('3', '4') ~ 'Anderson et al.: Large game',
+      anderson_smallgame ~ 'Anderson et al.: Small/medium game',
+      anderson_largegame ~ 'Anderson et al.: Large game',
       is.na(anderson_preysize) & anderson_hunt == 1 ~ 'Anderson et al.: Unknown size',
       is.na(anderson_preysize) & anderson_hunt == 0 ~ 'Anderson et al.: No hunting',
       .default = NA
     ),
     anderson_preysize2 = factor(anderson_preysize2, levels = c('Anderson et al.: Small/medium game', 'Anderson et al.: Large game', 'Anderson et al.: Unknown size', 'Anderson et al.: No hunting')),
-    small_game = factor(small_game, levels = c('No evidence', 'Never', 'Rarely', 'Sometimes', 'Frequently')),
-    large_game = factor(large_game, levels = c('No evidence', 'Never', 'Rarely', 'Sometimes', 'Frequently')),
+    psuedo_rep = str_detect(Pseudoreplication, "Yes"),
+    small_game = str_to_sentence(small_game),
+    large_game = str_to_sentence(large_game),
+    small_game = factor(small_game, levels = hunt_freq, ordered = T),
+    large_game = factor(large_game, levels = hunt_freq, ordered = T),
     women_hunt = small_game %in% c('Sometimes', 'Frequently') | large_game %in% c('Sometimes', 'Frequently'),
     women_hunt2 = small_game %in% c('Rarely', 'Sometimes', 'Frequently') | large_game %in% c('Rarely', 'Sometimes', 'Frequently'),
     false_negative = anderson_hunt == 0 & women_hunt2,
@@ -52,10 +70,55 @@ recode <-
     .after = Society
   )
 
-# Anderson et al. (2023) results
+#' # Inter-rater reliability
+
+rater2 <- 
+  read_excel("Intercoding_wh_20240130 SLL.xlsx", skip = 1) |> 
+  rename(
+    small_game2 = `Hunt small-medium game (<45kg)`,
+    large_game2 = `Hunt large game (≥45kg)`
+  ) |> 
+  mutate(
+    small_game2 = ifelse(small_game2 == 'NA', 'No evidence', small_game2),
+    large_game2 = ifelse(large_game2 == 'NA', 'No evidence', large_game2),
+    small_game2 = ifelse(small_game2 == '?', 'No evidence', small_game2), # Need to confirm this
+    large_game2 = ifelse(large_game2 == '?', 'No evidence', large_game2), # Need to confirm this
+    large_game2 = ifelse(large_game2 == 'never', 'Never', large_game2),
+    small_game2 = ifelse(small_game2 == 'Rare', 'Rarely', small_game2),
+    large_game2 = ifelse(large_game2 == 'Rare', 'Rarely', large_game2),
+    small_game2 = factor(small_game2, levels = c('No evidence', 'Never', 'Rarely', 'Sometimes', 'Frequently'), ordered = T),
+    large_game2 = factor(large_game2, levels = c('No evidence', 'Never', 'Rarely', 'Sometimes', 'Frequently'), ordered = T)
+  )
+
+# Fix some misspellings in rater2?
+
+rater2$Society[rater2$Society == 'Karuareg'] <- 'Kaurareg'
+rater2$Society[rater2$Society == 'Fish Lake Valley Northern Paiute'] <- 'Fish Lake Valley North Paiute'
+rater2$Society[rater2$Society == 'Torres Straight Islanders'] <- 'Torres Strait Islanders'
+setdiff(rater2$Society, recode$Society)
+
+rater2b <- 
+  left_join(rater2, recode[c('Society', 'small_game', 'large_game')]) |> 
+  dplyr::select(Society, small_game, small_game2, large_game, large_game2) |> 
+  na.omit()
+
+#+ fig.width=9, fig.height=9
+ragg::agg_png("Figures/agree_small.png", width = 800, height = 800)
+par(mai = c(1.5,1.5,1.5,1.5))
+b_small <- agreementplot(~small_game+small_game2, data = rater2b, main = 'Small game', xlab = 'SL', ylab = 'VV', ylab_rot = 0, ylab_just = 'right')
+dev.off()
+b_small
+
+ragg::agg_png("Figures/agree_large.png", width = 800, height = 800)
+par(mai = c(1.5,1.5,1.5,1.5))
+b_large <- agreementplot(~large_game+large_game2, data = rater2b, main = 'Large game', xlab = 'SL', ylab = 'VV', ylab_rot = 0, ylab_just = 'right')
+dev.off()
+b_large
+
+#' # Anderson et al. (2023) results
 
 # Most important activity (0=gathering, 1=hunting, 2=fishing, 3=hunt/gather)
-table(anderson$anderson_subsistence, useNA = 'a')
+table(anderson$anderson_subsistence2, useNA = 'a')
 
 # 0: No, 1: Yes
 table(anderson$anderson_hunt, useNA = 'a')
@@ -69,17 +132,21 @@ table(anderson$anderson_preysize, useNA = 'a')
 xtabs(~small_game+large_game, recode) |> kable()
 
 #+ fig.width=10
-ggplot(recode, aes(small_game, large_game)) +
+plot_recode <-
+  ggplot(recode, aes(small_game, large_game)) +
   geom_count() + 
   scale_size_area(breaks = c(1, 3, 5, 10, 15)) +
   labs(x = '\nSmall/medium game hunting (<45kg)', y = 'Large game hunting (≥45kg)') +
   guides(size = guide_legend('Number of societies')) +
   coord_fixed() +
   theme_bw()
+plot_recode
+ggsave("Figures/plot_recode.svg", plot_recode)
 
 # Table of societies by prey size and frequency
 
-recode |> 
+recode_table <-
+  recode |> 
   dplyr::select(Society, small_game, large_game, anderson_hunt) |> 
   mutate(Society = ifelse(anderson_hunt == 1, paste0('**', Society, '**'), Society)) |> 
   pivot_longer(c(small_game, large_game), names_to = 'Prey_size', values_to = 'Frequency') |> 
@@ -90,7 +157,7 @@ recode |>
   ) |> 
   pivot_wider(names_from = 'Prey_size', values_from = 'Societies') |> 
   relocate(small_game, .before = large_game) |> 
-  kable(col.names = c('Frequency', 'Women hunt small-medium game (<45kg)', 'Women hunt large game (≥45 kg)'))
+  kable(col.names = c('Frequency', 'Women hunt small-medium game (<45kg)', 'Women hunt large game (>45 kg)'))
 
 # Comparing Anderson et al. results to our results
 
@@ -102,11 +169,12 @@ recode |>
 # Our codes
 # HH = Hunter-Horticulturalists, A = Agriculturalists, H = Horiculturalists, HG = Hunter Gatherers, F = Fishers
 
-xtabs(~Subsistence+anderson_subsistence, recode, addNA = T) |> kable()
+xtabs(~Subsistence+anderson_subsistence2, recode, addNA = T) |> kable()
 
-# Anderson non-foragers
+# Anderson non-foragers (included in Figure 1)
 
-sum(recode$Subsistence != 'HG', na.rm = T) # Fishers not counted as foragers
+sum(!str_detect(recode$Subsistence, 'HG')) # Fishers, HH not counted as foragers
+sum(recode$Subsistence %in% c('A', 'H', 'HH')) # Fishers counted as foragers
 
 # Do women hunt? False negatives and false positives
 
@@ -121,16 +189,72 @@ sum(recode$false_positive1)
 # How many false positives according to looser criterion?
 sum(recode$false_positive2)
 
+recode$sg <- as.numeric(recode$small_game)
+recode$lg <- as.numeric(recode$large_game)
+pad <- 0.2
 
-#+ fig.width=10
-ggplot(recode, aes(small_game, large_game)) +
-  geom_count() + 
-  scale_size_area(breaks = c(1, 3, 5, 10)) +
-  labs(x = '\nSmall/medium game hunting (<45kg)', y = 'Large game hunting (≥45kg)') +
-  guides(size = guide_legend('Number of societies')) +
-  facet_wrap(~anderson_hunt2) + 
+anderson_no <- recode[recode$anderson_hunt==0,]
+anderson_fn <- anderson_no[anderson_no$false_negative,]
+xmin <- min(anderson_fn$sg) - pad
+xmax <- max(anderson_fn$sg) + pad
+ymin <- min(anderson_fn$lg) - pad
+ymax <- max(anderson_fn$lg) + pad
+
+lbl <- str_glue("False negatives (N={sum(anderson_no$false_negative)})")
+plot_false_neg <-
+  ggplot(anderson_no, aes(small_game, large_game)) +
+  geom_count() +
+  annotate("rect", xmin = xmin, ymin = ymin, xmax = xmax, ymax = ymax, colour = "blue", fill = NA) +
+  annotate("text", label = lbl, x = xmin, y = ymax+0.15, hjust=0) +
+  scale_size_area(breaks = c(1, 2, 5, 10), limits = c(1, 10)) +
+  scale_x_discrete(limits = levels(recode$small_game)) +
+  scale_y_discrete(limits = levels(recode$small_game)) +
+  labs(
+    title = "Anderson et al. (2023)",
+    subtitle = "No women hunting",
+    x = '\nSmall/medium game hunting (<45kg)', 
+    y = 'Large game hunting (≥45kg)'
+    ) +
+  guides(size = guide_none()) +
   coord_fixed() +
-  theme_bw()
+  theme_bw(15)
+plot_false_neg
+
+anderson_yes <- recode[recode$anderson_hunt==1,]
+anderson_fp <- anderson_yes[anderson_yes$false_positive1,]
+anderson_fp2 <- anderson_yes[anderson_yes$false_positive2,]
+xmin <- min(anderson_fp$sg) - pad
+xmax <- max(anderson_fp$sg) + pad
+xmax2 <- max(anderson_fp2$sg) + pad
+ymin <- min(anderson_fp$lg) - pad
+ymax <- max(anderson_fp$lg) + pad
+ymax2 <- max(anderson_fp2$lg) + pad
+
+lbl <- str_glue("False positives (N={sum(anderson_yes$false_positive1)})")
+lbl2 <- str_glue("False positives (N={sum(anderson_yes$false_positive2)})")
+plot_false_pos <-
+  ggplot(anderson_yes, aes(small_game, large_game)) +
+  geom_count() +
+  annotate("rect", xmin = xmin, ymin = ymin, xmax = xmax, ymax = ymax, colour='blue', fill = NA) +
+  annotate("text", label = lbl, x = xmin+0.1, y = ymax+0.15, hjust=0) +
+  annotate("rect", xmin = xmin, ymin = ymin, xmax = xmax2, ymax = ymax2, colour='blue', fill = NA) +
+  annotate("text", label = lbl2, x = xmin+0.1, y = ymax2+0.15, hjust=0) +
+  scale_size_area(breaks = c(1, 2, 5, 10), limits = c(1, 10)) +
+  scale_y_discrete(limits = levels(recode$small_game)) +
+  labs(
+    subtitle = "Women hunting",
+    x = '\nSmall/medium game hunting (<45kg)', 
+    y = 'Large game hunting (≥45kg)'
+    ) +
+  guides(size = guide_legend('Number of societies')) +
+  coord_fixed() +
+  theme_bw(15) + 
+  theme(axis.title.y = element_blank(), axis.text.y = element_blank())
+plot_false_pos
+
+plot_false_both <- plot_false_neg + plot_false_pos
+ggsave("Figures/plot_false_both.pdf", width = 14, height = 8)
+ggsave("Figures/plot_false_both.png", width = 14, height = 8)
 
 # Prey size inconsistencies
 
@@ -147,7 +271,8 @@ xtabs(~anderson_preysize2 + large_game, recode) |> kable()
 
 large_game_consistent <- sum(recode$anderson_preysize2 == 'Anderson et al.: Large game' & recode$large_game %in% c('Sometimes', 'Frequently'))
 large_game_consistent
-# large game inconsistent
+
+# large game inconsistent (included in Figure 1)
 sum(recode$anderson_preysize2 == 'Anderson et al.: Large game') - large_game_consistent
 
 # Anderson et al. unknown prey size
@@ -160,13 +285,36 @@ no_hunting_consistent
 # No hunting inconsistent
 sum(recode$anderson_preysize2 == 'Anderson et al.: No hunting') - no_hunting_consistent
 
+consistent <- tibble(
+  anderson_preysize2 = factor(levels(recode$anderson_preysize2), levels = levels(recode$anderson_preysize2)),
+  xmin = c(4, 1, NA, 1) - pad,
+  xmax = c(5, 5, NA, 2) + pad,
+  ymin = c(1, 4, NA, 1) - pad,
+  ymax = c(5, 5, NA, 2) + pad,
+  small_game = factor(hunt_freq[-1], levels = hunt_freq),
+  large_game = factor(hunt_freq[-1], levels = hunt_freq),
+  consistent = c(
+    sum(recode$small_game %in% c('Sometimes', 'Frequently') & recode$anderson_preysize2 == 'Anderson et al.: Small/medium game'),
+    sum(recode$large_game %in% c('Sometimes', 'Frequently') & recode$anderson_preysize2 == 'Anderson et al.: Large game'),
+    0,
+    sum(recode$small_game %in% c('No evidence', 'Never') & recode$large_game %in% c('No evidence', 'Never') & recode$anderson_preysize2 == 'Anderson et al.: No hunting')
+  ),
+  inconsistent = c(table(recode$anderson_preysize2) - consistent),
+  text = str_glue("Consistent: {consistent} Inconsistent: {inconsistent}")
+)
+
 #+ fig.width=10
-ggplot(recode, aes(small_game, large_game)) +
+plot_preysize <-
+  ggplot(recode, aes(small_game, large_game)) +
   geom_count() + 
   scale_size_area(breaks = c(1, 3, 5, 10)) +
+  geom_rect(data = consistent, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), colour = 'orange', fill = NA) +
+  geom_text(data = consistent, aes(label = text), x = 3, y = 5.4, hjust = 0.5) +
   labs(x = '\nSmall/medium game hunting (<45kg)', y = 'Large game hunting (≥45kg)\n') +
   guides(size = guide_legend('Number of societies')) +
   facet_wrap(~anderson_preysize2) + 
   coord_fixed() +
   theme_bw()
-
+plot_preysize
+ggsave("Figures/plot_preysize.pdf", plot_preysize, width = 10, height = 10)
+ggsave("Figures/plot_preysize.png", plot_preysize, width = 10, height = 10)
